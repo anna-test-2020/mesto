7e965e93c6e5ae882dbc83cb4e8b8c8f12b9423c
Anna Andrianova <andrianova.ru@yandex.ru>
2021-02-15 17:07:26 +0300

    2 попытка сдачи 4 работы

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,44 @@
+let profile = document.querySelector('.profile');
+// Выберите элементы, куда должны быть вставлены значения полей
+let profileName = profile.querySelector('.profile__name');
+let profileJob = profile.querySelector('.profile__occupation');
+
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit');
 let popupCloseButton = popup.querySelector('.popup__close');
 
-let togglePopup = function() {
-    popup.classList.toggle('popup_opened');
-}
-popupOpenButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
-
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
+let formElement = popup.querySelector('.popup__form');
+
 // Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_occupation');
 
+let togglePopup = function() {
+if(!popup.classList.contains('popup_opened')){
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    }
+else {
+    popup.classList.remove('popup_opened');
+    }
+}
+
 // Обработчик «отправки» формы
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.                                              
-
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Получите значение полей jobInput и nameInput из свойства value
     let nameVal = nameInput.value;
     let jobVal = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__occupation');
-
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameVal;
     profileJob.textContent = jobVal;
+    togglePopup();
 }
+
+popupOpenButton.addEventListener('click', togglePopup);
+popupCloseButton.addEventListener('click', togglePopup);
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', togglePopup);
